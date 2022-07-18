e2bf062806a55f3e47cc82f38d7b0989fa8fcae7
oneqrhee <blaackpurin@gmail.com>
2022-07-19 01:38:02 +0900

    5일차 알고리즘 문제풀이 (40번)

--- hh99_week2/src/com/coding/test/solution/day5/Solution40.java
+++ hh99_week2/src/com/coding/test/solution/day5/Solution40.java
@@ -1,8 +1,26 @@
 package com.coding.test.solution.day5;
 
+import java.util.Stack;
+
 public class Solution40 {
     public static int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        return answer;
+        Stack<Integer> result = new Stack<>();
+        int n = board.length;
+        int count = 0;
+        for (int move : moves) {
+            for (int j = 0; j < n; j++) {
+                if (board[j][move - 1] != 0) {
+                    if(!result.isEmpty() && result.peek() == board[j][move - 1]){
+                        result.pop();
+                        count += 2;
+                    }else{
+                        result.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
